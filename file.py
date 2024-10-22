fe59235bdb1a673947f00deb4bdcd5b3834a2cb6
Abdullah <harrisgold01@gmail.com>
2024-10-22 22:39:50 +0000

    update file.py

---
+++ file.py
@@ -0,0 +1,8 @@
+first = input ("First:")
+second = input ("Second:")
+sum = float (first) + float (second)
+print("Sum: " + str (sum))
+
+user_input = input("Enter your score:")
+score = float(user_input)
+print("Your score as a float is:", score)
